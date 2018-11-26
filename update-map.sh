64b0f0dfac8b9f298895b51461f30ce2652fad87
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-26 16:48:02 +0100

    Latest script version

--- update-map.sh
+++ update-map.sh
@@ -11,4 +11,4 @@ wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/src/p
 
 echo "Running IDP"
 IDP="/var/data/mapdata/idp/src/IDP/bin/release/netcoreapp2.1/linux-x64/IDP"
-$IDP --read-pbf /var/data/mapdata/belgium-latest.osm.pbf --pr --create-routerdb --elevation vehicles=/var/data/mapdata/bicycle.lua --contract "bicycle" --contract "bicycle.shortest" --contract "bicycle.balanced" --contract "bicycle.networks" --contract "bicycle.brussels" --contract "bicycle.relaxed" --write-routerdb belgium.routerdb
+$IDP --read-pbf /var/data/mapdata/belgium-latest.osm.pbf --pr --create-routerdb vehicles=/var/data/mapdata/bicycle.lua --elevation --contract "bicycle" --contract "bicycle.shortest" --contract "bicycle.balanced" --contract "bicycle.networks" --contract "bicycle.brussels" --contract "bicycle.relaxed" --write-routerdb belgium.routerdb
