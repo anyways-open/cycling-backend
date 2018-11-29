a92ed4202015519bc708e5afd45def5d869f34fb
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-29 14:15:06 +0100

    update-map.sh
    Made udpate script portable

--- update-map.sh
+++ update-map.sh
@@ -1,6 +1,5 @@
 # To be run on the server
 
-cd /var/data/mapdata
 echo "Downloading the latest map from geofabrik"
 rm belgium-latest.osm.pbf
 wget http://files.itinero.tech/data/OSM/planet/europe/belgium-latest.osm.pbf
@@ -10,7 +9,7 @@ rm bicycle.lua
 wget -O bicycle.lua https://raw.githubusercontent.com/anyways-open/routing-profiles/master/bicycle.lua?token=ABZgbuydBw_2nzVgLCaFU39i40OlEa8-ks5cBo4twA%3D%3D
 
 echo "Running IDP"
-IDP="/var/data/mapdata/idp/src/IDP/bin/release/netcoreapp2.1/linux-x64/IDP"
+IDP="./idp/src/IDP/bin/release/netcoreapp2.1/linux-x64/IDP"
 $IDP --read-pbf /var/data/mapdata/belgium-latest.osm.pbf --pr --create-routerdb vehicles=/var/data/mapdata/bicycle.lua --elevation --contract "bicycle" --contract "bicycle.shortest" --contract "bicycle.balanced" --contract "bicycle.networks" --contract "bicycle.brussels" --contract "bicycle.relaxed" --write-routerdb staged.belgium.routerdb
 
 # The routerdb is first written to staged.belgium.routerdb
