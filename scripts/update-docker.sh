f52281b23e3f5f30331e8d11e5055f635fb4d0cc
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-29 17:27:06 +0100

    KISS

--- scripts/update-docker.sh
+++ scripts/update-docker.sh
@@ -3,14 +3,14 @@
 
 # docker load -i cycling-backend.tar
 
-STATE=`docker pull anywaysopen/cycling-backend
-echo $STATE`
-if [[ $STATE = *"newer image"* ]]
+STATE=`docker pull anywaysopen/cycling-backend`
+echo $STATE | grep "newer update"
+if [[ $? ]]
 then
 
 	echo "Got a newer update. Deploying now..."
 	./run-docker.sh
 else
 	echo "Dockercontainter hasn't been updated, no need to redeploy"	
-
 fi
+echo $STATE
