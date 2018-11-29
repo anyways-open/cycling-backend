46f047a659b6aa1dbbceb713f848c0918994b38f
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-11-29 17:25:21 +0100

    Update docker script

--- scripts/update-docker.sh
+++ scripts/update-docker.sh
@@ -3,8 +3,9 @@
 
 # docker load -i cycling-backend.tar
 
-STATE=`docker pull anywaysopen/cycling-backend`
-if [[ $STATE = *"Status: Downloaded newer image for anywaysopen/cycling-backend:latest"* ]]
+STATE=`docker pull anywaysopen/cycling-backend
+echo $STATE`
+if [[ $STATE = *"newer image"* ]]
 then
 
 	echo "Got a newer update. Deploying now..."
