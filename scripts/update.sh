99a2c8e5000ad7473cc0944458034c35200fa149
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-01-16 17:00:31 +0100

    More robust update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,17 @@
+#! /bin/bash
+
+if [ ! -d cycling-backend ]
+then
+    git clone git@github.com:anyways-open/cycling-backend.git
+fi
+
+cd cycling-backend
+git pull
+cd ..
+
 rm *.sh
-wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/scripts/update.sh
-wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/scripts/update-map.sh
-wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/scripts/create-docker.sh
-wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/scripts/update-docker.sh
-wget https://raw.githubusercontent.com/anyways-open/cycling-backend/master/scripts/run-docker.sh
-
-
+cp cycling-backend/scripts/* .
 chmod u+x *.sh
-
 
 ./update-map.sh
 
